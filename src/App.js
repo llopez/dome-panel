c5f3f562cfb5a105bba41796faf3fb8b7020a48a
Luis Lopez <luigibyte@gmail.com>
2019-04-23 00:17:42 -0300

    Add very basic Item component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Item from './components/Item'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Item name="Light 1" />
+      <Item name="Light 2" />
+      <Item name="Light 3" />
     </div>
   );
 }
---
+++ src/components/Item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Item = (props) => (
+  <li>{props.name}</li>
+);
+
+export default Item;
